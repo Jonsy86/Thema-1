bfc4ac637a4fa6fccdff3a6cc7c197d6c5801e21
Jonas_Nolte <jonasnolte86@gmail.com>
2024-05-08 12:00:09 +0200

    Variable und Konstante

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -46,3 +46,16 @@
 // console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
 // console.log("Datentyp " + typeof test); // Ausgabe Datentyp)
 // console.log("-------------------")
+
+/* Variablen vs. Konstanten in JS */
+
+// Variable
+let test; // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe 
+
+// Konstante
+const test2 = "Hi!";  // Deklaration + Wertzuweisung gemeinsam !
+test2 = "hello"; // Laufzeitfehler !!
+console.log("Inhalt: " + test2); // Ausgabe 
