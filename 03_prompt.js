f88f5dd2cb93bdc3363655f045c69a21b289cbb4
Jonas_Nolte <jonasnolte86@gmail.com>
2024-05-08 10:36:16 +0000

    update

--- 03_prompt.js
+++
@@ -1,6 +0,0 @@
-const prompt = require('prompt-sync')({sigint: true});
-
-// Alles muss man selber machen lassen! ...
-let firstName = prompt("Vorname?: ");
-let familyName = prompt("Name?: ");
-console.log("Hallo, " + firstName + " " + familyName +"!");
