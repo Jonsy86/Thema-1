198aaa9c09f336b56c81858448b4adf6106c868b
Jonas_Nolte <jonasnolte86@gmail.com>
2024-05-10 09:35:35 +0000

    up

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -2,7 +2,7 @@
 
 // Deklaration + Assignment
 const ageJohn = 30;
-const ageMark = 30;
+const ageMark = 25;
 
 // Deklaration
 let isJohnOlder, isJohnEqual;
@@ -11,10 +11,10 @@ let isJohnOlder, isJohnEqual;
 isJohnOlder = (ageJohn > ageMark);
 isJohnEqual = (ageJohn == ageMark);
 
-// Ausgabe
-// console.log("ageJohn: " + ageJohn);
-// console.log("ageMark: " + ageMark);
-// console.log("isJohnOlder: " + isJohnOlder);
-// console.log("isJohnEqual: " + isJohnEqual);
-// console.log("------------------");
+//  Ausgabe
+ console.log("ageJohn: " + ageJohn);
+ console.log("ageMark: " + ageMark);
+ console.log("isJohnOlder: " + isJohnOlder);
+ console.log("isJohnEqual: " + isJohnEqual);
+ console.log("------------------");
 
