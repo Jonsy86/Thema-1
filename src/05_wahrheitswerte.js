656b5c1f7a65674606dba335217897ae80ee2ecb
Jonas_Nolte <jonasnolte86@gmail.com>
2024-05-10 08:57:53 +0000

    update

--- src/05_wahrheitswerte.js
+++ src/05_wahrheitswerte.js
@@ -7,7 +7,8 @@ const birthYearJohn = 1980;
 const birthYearMark = 1990;
 
 // Berechnung Alter
-let year = 2024;
+let date = new Date();
+let year = date.getFullYear();
 ageJohn = year - birthYearJohn;
 ageMark = year - birthYearMark;
 
@@ -15,3 +16,10 @@ ageMark = year - birthYearMark;
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
 
+
+// Tests
+let isJohnOlder, isJohnEqual // Is John older? --> y/n --> true/false
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark)
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual)
