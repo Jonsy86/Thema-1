2dfaffebe7862cc0b48246315f319d4df8013806
Jonas_Nolte <jonasnolte86@gmail.com>
2024-05-10 10:57:10 +0000

    update if else if

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -1,27 +1,55 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assignment
-const ageJohn = 30;
-const ageMark = 35;
+// const ageJohn = 30;
+// const ageMark = 35;
 
 // Deklaration
-let isJohnOlder, isJohnEqual;
+// let isJohnOlder, isJohnEqual;
 
 // Test | Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+// isJohnOlder = (ageJohn > ageMark);
+// isJohnEqual = (ageJohn == ageMark);
 
 //  Ausgabe
- console.log("ageJohn: " + ageJohn);
- console.log("ageMark: " + ageMark);
- console.log("isJohnOlder: " + isJohnOlder);
- console.log("isJohnEqual: " + isJohnEqual);
- console.log("------------------");
+//  console.log("ageJohn: " + ageJohn);
+//  console.log("ageMark: " + ageMark);
+//  console.log("isJohnOlder: " + isJohnOlder);
+//  console.log("isJohnEqual: " + isJohnEqual);
+//  console.log("------------------");
 
 // if (false) //fake it!
 // if (isJohnOlder) // Verweis auf ein Testergebnis
-if(ageJohn > ageMark) // Test 
+// if(ageJohn > ageMark) // Test 
 
 { // codeblock
-    console.log("John ist älter!");   
+    // console.log("John ist älter!");   
 }
+
+// IF - Else
+// Mit ALternative, entweder ja oder nein
+
+// if (condition) 
+// { // ja-zweig
+//     console.log("John ist älter!");   
+// }
+// else 
+// { // alternativ-zweig
+//     console.log("John ist jünger!");    
+// }
+
+// IF - Else If
+
+// if (isJohnOlder) 
+// {
+//   console.log("John ist älter!")  
+// }
+// else if (isJohnEqual) 
+// {
+//  console.log("John ist gleich alt!")   
+// }
+
+// else 
+// {
+//    console.log("John ist jünger!") 
+// }
