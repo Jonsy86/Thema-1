3327941d61995e02b31180b00d4888d212647b5c
Jonas_Nolte <jonasnolte86@gmail.com>
2024-05-13 08:51:51 +0000

    new

---
+++ src/08_funktionen.js
@@ -0,0 +1,15 @@
+
+/***** Funktionen 01 *****/
+// 1. Kapselung von Codeblöcken
+
+
+// Funktionsrumpf callee
+// Funktionsdeklaration
+function test() 
+{
+console.log("Hallo Esther!")    
+}
+
+test();
+
+
