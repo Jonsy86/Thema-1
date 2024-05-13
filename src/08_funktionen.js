0ec646d8c4e2513782d180716a5ac40e7cb29494
Jonas_Nolte <jonasnolte86@gmail.com>
2024-05-13 10:20:07 +0000

    gkjhk

--- src/08_funktionen.js
+++ src/08_funktionen.js
@@ -5,12 +5,12 @@
 
 // Funktionsrumpf callee
 // Funktionsdeklaration
-function test() 
-{
-console.log("Hallo Esther!")    
-}
+// function test() 
+// {
+// console.log("Hallo Esther!")    
+// }
 
-test();
+// test();
 
 /***** Funktionen 02a *****/
 // 2a. Parametrisierung + Datenübergabe von INNEN
@@ -19,13 +19,28 @@ test();
 // outputNames();
 
 // Funktionsdeklaration
-function outputNames() 
+// function outputNames() 
 {
      // interne Variable | What happens in VEGAS ...
-    let firstName = "Jan";
-    console.log("Hallo, " + firstName + "!"); 
+    // let firstName = "Jan";
+    // console.log("Hallo, " + firstName + "!"); 
 }
 
 // console.log(firstName); // Fehler : scope!
+
+
+
+
+outputNames3("Maxine","Mützerich");
+
+const prompt = require('prompt-sync')({sigint: true});
+outputNames3(prompt("Vorname? :"),prompt("Nachname?:"))
+
+function outputNames3(firstName, familyName) 
+{
+    console.log("Hallo, " + firstName + " " + familyName + "!")
+} 
+
+
 
 
