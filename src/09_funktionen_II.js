1ca11674c59083e27a6cfea570f8f4dacfea380a
Jonas_Nolte <jonasnolte86@gmail.com>
2024-05-14 11:46:27 +0200

    Update 09_funktionen_II.js

--- src/09_funktionen_II.js
+++ src/09_funktionen_II.js
@@ -29,7 +29,30 @@ function add(a,b) {
 	return a + b;
 }
 
+// module: multiplication a * b |  test:
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+output(divide(0,2));
+output(divide(3,0));
+output(divide(0,0));
+function divide(a,b) {
 
+	if (b !== 0) {
+		return a / b;
+	} else {
+		return "Error: Divided by 0!";
+	}
+   
+}
+
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+function multiply(a,b) {
+    return a * b;
+}
 
 
 
